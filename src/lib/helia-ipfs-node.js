0b3c22755afe34ae1c4a7e1872fca1132c34ee25
erickgonzalezdev <erickgonzalezdev>
2024-08-09 01:41:54 -0400

    added pin rpc

--- src/lib/helia-ipfs-node.js
+++ src/lib/helia-ipfs-node.js
@@ -1,30 +1,67 @@
-import { HeliaNode as Node, Server } from 'helia-ipfs-node/src/index.js'
+import { HeliaNode as Node, Server, PinRPC } from 'helia-ipfs-node/src/index.js'
 
 class HeliaNode {
   constructor (config = {}) {
+    if (!config.dbModels) {
+      throw new Error('dbModels must be passed in constructor when instatiate HeliaNode lib.')
+    }
     this.config = config
+    this.dbModels = this.config.dbModels
     this.HeliaNode = Node
     this.HeliaServer = Server
+    this.PinRPC = PinRPC
     this.node = null
     this.gateway = null
+    this.rpc = null
     this.wlogger = this.config.wlogger
 
     // Bind function
     this.start = this.start.bind(this)
+    this.onSuccessRemotePin = this.onSuccessRemotePin.bind(this)
   }
 
   async start () {
     try {
       this.node = new this.HeliaNode(this.config)
       await this.node.start()
-      console.log('node started!')
+      this.wlogger.info('node started!')
 
       // Start Gateway.
       this.gateway = new this.HeliaServer({ node: this.node, port: this.config.gatewayPort })
       await this.gateway.start()
+
+      this.rpc = new this.PinRPC({
+        node: this.node,
+        topic: this.config.rpcTopic,
+        onSuccessRemotePin: this.onSuccessRemotePin
+      })
+      await this.rpc.start()
     } catch (error) {
       this.wlogger.error(`Error in helia-node-ipfs/start() $ ${error.message}`)
       throw error
+    }
+  }
+
+  async onSuccessRemotePin (data = {}) {
+    try {
+      const { cid, host } = data
+      if (!cid || typeof cid !== 'string') throw new Error('cid must be a string!')
+      if (!host || typeof host !== 'string') throw new Error('host must be a string!')
+
+      this.wlogger.info('success remote pin ', data)
+
+      const pin = await this.dbModels.Pin.findOne({ cid })
+      if (!pin) throw new Error('pin not found!')
+
+      pin.pinned = true
+      pin.host.push(host)
+      await pin.save()
+
+      return pin
+    } catch (error) {
+      this.wlogger.error('Error on onSuccessRemotePin() ', error)
+      // skip error
+      return false
     }
   }
 }
--- test/unit/mocks/helia-node-mock.js
+++ test/unit/mocks/helia-node-mock.js
@@ -1,11 +1,56 @@
 // Mocks for helia-ipfs-node module
 
+// Mocks for libp2p js package node module.
+class Pubsub {
+  constructor (config) { this.config = config }
+
+  async subscribe () { return true }
+  async publish () { return true }
+  async addEventListener () { return true }
+}
+class ContentRoutingMock {
+  constructor (config) { this.config = config }
+
+  async findProviders () { return true }
+  async getConnections () { return [] }
+}
+
+class Libp2pMock {
+  constructor () {
+    this.contentRouting = new ContentRoutingMock()
+    this.services = {
+      pubsub: new Pubsub()
+    }
+  }
+
+  getMultiaddrs () { return [] }
+  async dial () { return true }
+  async getConnections () { return [] }
+}
+
 class HeliaNodeMock {
-  constructor (config = {}) { this.condig = config }
+  constructor (config = {}) {
+    this.peerId = 'testPeerId'
+    this.config = config
+    this.helia = {
+      libp2p: new Libp2pMock()
+    }
+  }
+
   async start () {}
 
   async pinCid () { return 'cid' }
   async uploadFile () { return 'cid' }
+}
+
+class PinRPCMock {
+  constructor (config = {}) {
+    this.config = config
+  }
+
+  async start () {}
+
+  async requestRemotePin () { return 'cid' }
 }
 
 class HeliaServerMock {
@@ -20,4 +65,4 @@ const FileMock = {
   name: 'text.txt'
 }
 
-export { HeliaNodeMock, HeliaServerMock, FileMock }
+export { HeliaNodeMock, HeliaServerMock, PinRPCMock, FileMock }
